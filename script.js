171c5bd9b323d23f8fa124e62e1a0fb70f35ea09
MrTaciturno <leo.age@gmail.com>
2024-12-09 15:22:33 -0300

    roll

--- script.js
+++ script.js
@@ -7,7 +7,35 @@ function rolarDados(max){
 
 
 
-// Funcionou perfeitamente. Mas e se quisermos exibir na tela todos os dados do senhor Alfredo, os que definimos dentro do nosso objeto? Para fazer isso, podemos utilizar o texto entre crases (`) em vez de aspas.
+
+
+
+
+
+
+
+// //cria 10 Joes iguais
+//const Army = [];
+
+// for (let i = 0; i < 10; i++){
+//     Army.push(new Personagem(`Joe${i}`, 3, 0));
+// }
+
+        //cria todas a permutações de atributo
+        // let a = 0;
+        // for (let i = 0; i <= 3; i++){
+        //     for(let j = (3-i); j <= 3; j++){
+        //         Army.push(new Personagem(`Joe${a}`, i, j, 4));
+        //         a++;
+        //     }
+        // }
+
+
+
+
+
+
+//  se quisermos exibir na tela todos os dados do senhor Alfredo, os que definimos dentro do nosso objeto? Para fazer isso, podemos utilizar o texto entre crases (`) em vez de aspas.
 
 // Ao colocar nosso texto entre crases, o JavaScript nos permite utilizar a seguinte estrutura: “cifrão” seguido “abre e fecha chaves” (${}). Com isso, podemos inserir códigos e valores dinâmicos dentro do nosso texto.
 
